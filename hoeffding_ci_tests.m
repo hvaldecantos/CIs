b138fd860e2319bf184eea933b23ccad8a9d49c7
Héctor A. Valdecantos <hvaldecantos@gmail.com>
2018-11-24 13:29:43 -0500

    removes unneeded array

--- hoeffding_ci_tests.m
+++ hoeffding_ci_tests.m
@@ -9,14 +9,12 @@ end
 function hoeffding_ci_test(xn, alpha, yn)
 
     theta = 0.5;
-    y_sample = [];
     epsilon = sqrt((1/(2*xn))*log(2/alpha));
     captured_mean = 0;
 
     for i = 1:yn
         x_sample = sample_bernoulli(xn, theta);
         x_bar = mean(x_sample);
-        y_sample = [y_sample; x_bar];
 
         if(abs(x_bar - theta) <= epsilon)
             captured_mean = captured_mean + 1;
